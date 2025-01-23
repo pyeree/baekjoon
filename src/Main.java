a54531c6b109b1f5507264862c6d6569c398ddab
pyeree <dlagusdn0218@gmail.com>
2025-01-23 17:45:40 +0900

    java baekjoon 2609

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,31 @@
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
-import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		// 첫 번째 줄: n 입력 받기
-		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-		// 배열 선언
-		int[] arr = new int[n];
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		// 최대 공약수 유클리드 호제법
+		// a*b = 최대공약수 * 최소공배수
+		int GCD = gcd(a, b);
+		int lcm = (a * b) / GCD;
+		System.out.println(GCD);
+		System.out.println(lcm);
 
-		// 두 번째 줄부터 n개의 숫자를 입력 받기
-		for (int i = 0; i < n; i++) {
-			arr[i] = Integer.parseInt(br.readLine());
+	}
+
+	public static int gcd(int a, int b) {
+		while (b != 0) {
+			int temp = a % b;
+			a = b;
+			b = temp;
+			gcd(a, b);
 		}
-		br.close();
-		Arrays.sort(arr);
-		for (int i = 0; i < n; i++) {
-			sb.append(arr[i]).append('\n');
-		}
-		// Arrays.sort()를 사용하여 오름차순 정렬
-
-		// 정렬된 배열 출력
-		System.out.println(sb);
-
-
+		return a;
 	}
 }
