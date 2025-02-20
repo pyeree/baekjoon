837646e6324673148f271bde4978f8c5c711ae5a
pyeree <dlagusdn0218@gmail.com>
2025-02-20 11:49:35 +0900

    20250220_bakejun_11399&2606_S3

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static Integer[] dp;
+	static boolean[] check;
+	static int[][] arr;
+	static int count = 0;
 
-	public static int recru(int n) {
+	static int node, line;
 
-		if (dp[n] == null) {
-			if (n % 6 == 0) {
-				dp[n] = Math.min(recru(n - 1), Math.min(recru(n / 2), recru(n / 3))) + 1;
-			} else if (n % 3 == 0) {
-				dp[n] = Math.min(recru(n / 3), recru(n - 1)) + 1;
-			} else if (n % 2 == 0) {
-				dp[n] = Math.min(recru(n / 2), recru(n - 1)) + 1;
-			} else {
-				dp[n] = recru(n - 1) + 1;
-			}
-		}
-
-		return dp[n];
-	}
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 
-		int n = Integer.parseInt(in.readLine());
+		node = Integer.parseInt(in.readLine());
+		line = Integer.parseInt(in.readLine());
 
-		dp = new Integer[n + 1];
-		dp[0] = dp[1] = 0;
+		arr = new int[node+1][node+1];
+		check = new boolean[node+1];
+		for (int i = 0; i < line; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine());
 
-		System.out.println(recru(n));
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 
+			arr[a][b] = arr[b][a] = 1;
+		}
+
+		dfs(1);
+		System.out.println(count-1);
+
+	}
+
+	public static void dfs(int start) {
+		check[start] = true;
+		count++;
+		for (int i = 0; i <= node; i++) {
+			if (arr[start][i] == 1 && !check[i]) {
+				dfs(i);
+			}
+		}
 	}
 }
